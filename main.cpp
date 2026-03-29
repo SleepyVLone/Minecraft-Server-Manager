178eb8b5859fa328363003a6a00d45195f29cc3f
SleepyVLone <sleepycb17@gmail.com>
2026-03-29 05:48:47 +0100

    A simple c++ command line tool for managing minecraft servers

---
+++ main.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <fstream>  // for reading files
+#include <sstream>  // for splitting strings
+
+using namespace std;
+
+int main() 
+{
+    string serverPath = "server.jar"; //Used if the config file is missing
+
+    int ram = 2; //set RAM to 2G if the config file is missing
+
+    ifstream configFile("config.txt"); //Reads the config file
+    string line;
+
+    while (getline(configFile, line))
+    {
+        stringstream ss(line);
+        string key, value;
+
+        getline(ss, key, '='); //read up to the =
+        getline(ss, value); //read the rest
+
+        if (key == "ram") ram = stoi(value);
+        if (key == "server_path") serverPath = value;
+    }
+
+    configFile.close();
+
+    int choice;
+
+    while (true) 
+    {
+        cout << "=== Minecraft Server Manager ===" << endl << endl;
+
+        cout << "1. Start Server" << endl;
+        cout << "2. Stop Server" << endl;
+        cout << "3. Backup Server" << endl;
+        cout << "4. View Player Count" << endl;
+        cout << "5. Exit" << endl;
+
+        cin >> choice;
+
+        if (choice == 1)
+        {
+            cout << "Starting server..." << endl;
+    
+            string command = "java -Xmx" + to_string(ram) + "G -Xms1G -jar " + serverPath + " nogui";
+            system(command.c_str());
+    
+            cout << "Server stopped." << endl;
+        }
+        else if (choice == 2)
+        {
+
+        }
+        else if (choice == 3)
+        {
+
+        }
+        else if (choice == 4)
+        {
+
+        }
+        else if (choice == 5)
+        {
+            cout << "Goodbye!" << endl;
+            
+            break;
+        }  
+        else
+        {
+            cout << "Selection invalid. Please make a valid selection.";
+        }
+
+    }
+
+    return 0;
+}
